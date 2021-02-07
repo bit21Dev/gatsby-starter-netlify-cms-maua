3f29aafdc29a3ce29a408dd892be67126a87a3f2
Daniel Felipe <daniel.companyservicostecnicos@gmail.com>
2021-02-07 13:52:59 -0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,17 @@ import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
   render() {
-   
+    return (
+      <footer className="footer has-background-black has-text-white-ter">
+        <div className="content has-text-centered">
+          <img
+            src={logo}
+            alt="Kaldi"
+            style={{ width: '14em', height: '10em' }}
+          />
+        </div>
+      </footer>
+    )
   }
 }
 
